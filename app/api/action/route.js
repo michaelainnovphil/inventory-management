16d51d85334f91a58822f25cf344b27f46a518f4
michaelainnovphil <mdbarreda@innovphil.com>
2025-07-15 16:27:36 +0800

    try 2: fixed action/route

--- app/api/action/route.js
+++ app/api/action/route.js
@@ -1,41 +1,44 @@
 import connectToMongo from '../../../db/dbConnect';
-import Product from "/db/models/Products";
+import Product from "@/db/models/Products";
 import { verify } from "jsonwebtoken";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
-  const requestHeaders = new Headers(request.headers);
-  const token = requestHeaders.get("auth-token");
-  const data = verify(token, process.env.JWT_SECRET);
-
-  let id = data.user.id;
-
   try {
+    const requestHeaders = new Headers(request.headers);
+    const token = requestHeaders.get("auth-token");
+    const data = verify(token, process.env.JWT_SECRET);
+
     await connectToMongo();
-    let { action, slug, initialQuantity } = await request.json();
+    const { action, slug, initialQuantity } = await request.json();
 
     let newQuantity =
-      action == "plus"
+      action === "plus"
         ? parseInt(initialQuantity) + 1
         : parseInt(initialQuantity) - 1;
 
     if (newQuantity < 0) {
       return NextResponse.json({
         success: false,
-        message: "Product quantity cannot be negative!",
+        message: "Quantity cannot be negative.",
       });
     }
 
-    const product = await Product.findOne({
-      $and: [{ user: id }, { slug: slug }],
-    });
-    product.quantity = newQuantity;
+    const product = await Product.findOne({ slug });
 
+    if (!product) {
+      return NextResponse.json({
+        success: false,
+        message: "Product not found.",
+      });
+    }
+
+    product.quantity = newQuantity;
     const savedProduct = await product.save();
 
-    return NextResponse.json({ savedProduct, success: true });
+    return NextResponse.json({ success: true, savedProduct });
   } catch (error) {
-    console.error(error.message);
+    console.error("Error updating quantity:", error.message);
     return new NextResponse(
       JSON.stringify({ success: false, message: error.message }),
       { status: 500, headers: { "content-type": "application/json" } }
